42ce79b07452c780f0b991341e4c83b592e24ea5
Ana Clara Ribeiro <anaclaraor@outlook.com>
2025-04-28 15:38:59 -0300

    first commit

---
+++ test_realiza_compra_1.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.implicitly_wait(5)
+
+driver.get("https://saucedemo.com")
+
+# Login
+driver.find_element(By.ID, "user-name").send_keys("standard_user")
+driver.find_element(By.ID, "password").send_keys("secret_sauce")
+driver.find_element(By.ID, "login-button").click()
+
+# Adicionando o primeiro produto ao carrinho
+driver.find_element(By.XPATH, "//*[@class='inventory_item_name ' and text()='Sauce Labs Backpack']").click()
+driver.find_element(By.XPATH, "//*[text()='Add to cart']").click()
+
+# Verificando o carrinho
+driver.find_element(By.XPATH, "//*[@class='shopping_cart_link']").click()
+assert driver.find_element(By.XPATH, "//*[text()='Sauce Labs Backpack']").is_displayed()
+
+# Dar continuidade na compra
+driver.find_element(By.ID, "checkout").click()
+
+# Informa os dados do comprador
+driver.find_element(By.ID, "first-name").send_keys("Ana")
+driver.find_element(By.ID, "last-name").send_keys("Kiss")
+driver.find_element(By.ID, "postal-code").send_keys("12345678")
+driver.find_element(By.ID, "continue").click()
+
+# Valida o resumo de compra
+assert driver.find_element(By.XPATH, "//*[text()='Sauce Labs Backpack']").is_displayed()
+
+# Finaliza a compra
+driver.find_element(By.ID, "finish").click()
+assert driver.find_element(By.XPATH, "//*[text()='Thank you for your order!']").is_displayed()
