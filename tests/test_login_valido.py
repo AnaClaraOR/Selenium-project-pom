8cee3760c0f1ad4ac61e3a1fdbd0c58c62c25f10
Ana Clara Ribeiro <anaclaraor@outlook.com>
2025-05-03 19:23:33 -0300

    Criação da home_page como melhoria

--- tests/test_login_valido.py
+++ tests/test_login_valido.py
@@ -1,14 +1,17 @@
 import pytest
-import conftest
-from selenium.webdriver.common.by import By
+
+from pages.home_page import HomePage
 from pages.login_page import LoginPage
 
 @pytest.mark.usefixtures("setup_teardown")
 class TestCT01:
     def test_ct01_login_valido(self):
-        driver = conftest.driver
+        # Instacia os objetos a serem usados no teste
         login_page = LoginPage()
+        home_page = HomePage()
 
+        # Faz login
         login_page.fazer_login("standard_user", "secret_sauce")
 
-        assert driver.find_element(By.XPATH, "//span[@class='title']").is_displayed()
+        # Verifica se o login foi realizado
+        home_page.verificar_login_com_sucesso()
