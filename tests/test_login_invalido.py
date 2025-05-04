3425cf51e1b3e3a3b8eddd39eae1024894530e39
Ana Clara Ribeiro <anaclaraor@outlook.com>
2025-05-04 11:36:55 -0300

    Verifica o texto de um elemento

--- tests/test_login_invalido.py
+++ tests/test_login_invalido.py
@@ -5,6 +5,8 @@ from pages.login_page import LoginPage
 @pytest.mark.usefixtures("setup_teardown")
 class TestCT02:
     def test_ct02_login_invalido(self):
+        mensagem_erro_esperada = "Epic sadface: Username and password do not match any user in this service"
+
         # Instancia os objetos a serem utilizados nos testes
         login_page = LoginPage()
 
@@ -12,4 +14,7 @@ class TestCT02:
         login_page.fazer_login("standard_user", "12345")
 
         # Verifica se o login não foi realizado e a mensagem de erro apresentada
-        login_page.verificar_mensagem_erro_login()
+        login_page.verificar_mensagem_erro_login_existe()
+
+        # Verifica o texto da mensagem de erro
+        login_page.verificar_texto_mensagem_erro_login(mensagem_erro_esperada)
