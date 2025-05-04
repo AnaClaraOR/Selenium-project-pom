eb9939ac67c9c6932f93fc325abbefca6a21989f
Ana Clara Ribeiro <anaclaraor@outlook.com>
2025-05-04 11:05:55 -0300

    Melhoria no login invalido

--- tests/test_login_invalido.py
+++ tests/test_login_invalido.py
@@ -1,15 +1,15 @@
 import pytest
-import conftest
-from selenium.webdriver.common.by import By
 from pages.login_page import LoginPage
 
 
 @pytest.mark.usefixtures("setup_teardown")
 class TestCT02:
     def test_ct02_login_invalido(self):
-        driver = conftest.driver
+        # Instancia os objetos a serem utilizados nos testes
         login_page = LoginPage()
 
+        # Faz o login
         login_page.fazer_login("standard_user", "12345")
 
-        assert len(driver.find_elements(By.XPATH, "//span[@class='title']")) == 0
+        # Verifica se o login não foi realizado e a mensagem de erro apresentada
+        login_page.verificar_mensagem_erro_login()
