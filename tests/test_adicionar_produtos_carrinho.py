22b9074fa00e12f6bbd4f9efe8f57c9539b983a5
Ana Clara Ribeiro <anaclaraor@outlook.com>
2025-05-04 12:38:31 -0300

    Melhoria no scrip de adicionar o produto ao carrinho

--- tests/test_adicionar_produtos_carrinho.py
+++ tests/test_adicionar_produtos_carrinho.py
@@ -1,33 +1,37 @@
 import pytest
 import conftest
-from selenium.webdriver.common.by import By
+
+from pages.carrinho_page import CarrinhoPage
+from pages.home_page import HomePage
 from pages.login_page import LoginPage
 
 @pytest.mark.usefixtures("setup_teardown")
 class TestCT03:
     def test_ct03_adicionar_produtos_carrinho(self):
-        driver = conftest.driver
         login_page = LoginPage()
+        home_page = HomePage()
+        carrinho_page = CarrinhoPage()
 
-        # Login
+        produto_1 = "Sauce Labs Backpack"
+        produto_2 = "Sauce Labs Bolt T-Shirt"
+
+        # Faz o login
         login_page.fazer_login("standard_user", "secret_sauce")
 
         # Adicionando o primeiro produto ao carrinho
-        driver.find_element(By.XPATH, "//*[@class='inventory_item_name ' and text()='Sauce Labs Backpack']").click()
-        driver.find_element(By.XPATH, "//*[text()='Add to cart']").click()
+        home_page.adicionar_ao_carrinho(produto_1)
 
-        # Verificando o carrinho
-        driver.find_element(By.XPATH, "//*[@class='shopping_cart_link']").click()
-        assert driver.find_element(By.XPATH, "//*[text()='Sauce Labs Backpack']").is_displayed()
+        # Verificando o produto no carrinho
+        home_page.acessar_carrinho()
+        carrinho_page.verificar_produto_carrinho_existe(produto_1)
 
         # Retornando aos produtos
-        driver.find_element(By.ID, "continue-shopping").click()
+        carrinho_page.clicar_continuar_comprando()
 
         # Adicionando um novo produto ao carrinho
-        driver.find_element(By.XPATH, "//*[@class='inventory_item_name ' and text()='Sauce Labs Bolt T-Shirt']").click()
-        driver.find_element(By.XPATH, "//*[text()='Add to cart']").click()
+        home_page.adicionar_ao_carrinho(produto_2)
 
         # Verificando o carrinho novamente com vários produtos
-        driver.find_element(By.XPATH, "//*[@class='shopping_cart_link']").click()
-        assert driver.find_element(By.XPATH, "//*[text()='Sauce Labs Backpack']").is_displayed()
-        assert driver.find_element(By.XPATH, "//*[text()='Sauce Labs Bolt T-Shirt']").is_displayed()
+        home_page.acessar_carrinho()
+        carrinho_page.verificar_produto_carrinho_existe(produto_1)
+        carrinho_page.verificar_produto_carrinho_existe(produto_2)
